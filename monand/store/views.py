a7e05335568f1074676bef0466de5c38b041ee12
Kairat Sultan <garri.sultonov@mail.ru>
2023-03-02 20:37:21 +0600

    fix location endpoint view

--- monand/store/views.py
+++ monand/store/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets, filters, status, generics
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from geopy.geocoders import Nominatim
 
 from .paginators import CustomPagination
 from .serializers import *
